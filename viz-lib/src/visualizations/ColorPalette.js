f20dff9620a24affcb1600a9463e8334d58e0a82
Simon Gurcke <simon@gurcke.de>
2020-07-15 23:56:56 +1000

    GC colours

--- viz-lib/src/visualizations/ColorPalette.js
+++ viz-lib/src/visualizations/ColorPalette.js
@@ -2,30 +2,20 @@ import { values } from "lodash";
 
 // The following colors will be used if you pick "Automatic" color
 export const BaseColors = {
-  Blue: "#356AFF",
-  Red: "#E92828",
-  Green: "#3BD973",
-  Purple: "#604FE9",
-  Cyan: "#50F5ED",
-  Orange: "#FB8D3D",
-  "Light Blue": "#799CFF",
-  Lilac: "#B554FF",
-  "Light Green": "#8CFFB4",
-  Brown: "#A55F2A",
-  Black: "#000000",
-  Gray: "#494949",
-  Pink: "#FF7DE3",
-  "Dark Blue": "#002FB4",
+  "Life Green": "#05bf6f",
+  "Cerulean Blue": "#12a1dc",
+  "Coral Pink": "#eb5d66",
+  "Earth Orange": "#f18b31",
+  "Appleberry Mauve": "#574b99",
+  "Sunrise Violet": "#c66aa6",
+  "Sage Green": "#007965",
+  "Warm Stone": "#726e6a",
+  "Pale Stone": "#cfcbc8",
 };
 
 // Additional colors for the user to choose from
 export const AdditionalColors = {
-  "Indian Red": "#981717",
-  "Green 2": "#17BF51",
-  "Green 3": "#049235",
-  "Dark Turquoise": "#00B6EB",
-  "Dark Violet": "#A58AFF",
-  "Pink 2": "#C63FA9",
+  "White": "#fafafa",
 };
 
 export const ColorPaletteArray = values(BaseColors);

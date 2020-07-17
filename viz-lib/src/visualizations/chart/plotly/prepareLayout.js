2e28eced9204427cbfc3b79cce30367f36271fce
Simon <simon@gurcke.de>
2020-07-17 13:33:01 +1000

    Add default margin

--- viz-lib/src/visualizations/chart/plotly/prepareLayout.js
+++ viz-lib/src/visualizations/chart/plotly/prepareLayout.js
@@ -94,8 +94,9 @@ function prepareDefaultLayout(layout, options, data) {
   const y2Series = data.filter(s => s.yaxis === "y2");
 
   layout.xaxis = prepareXAxis(options.xAxis, options);
-
   layout.yaxis = prepareYAxis(options.yAxis[0], options, ySeries);
+  layout.margin = {l: 50, r: 50};
+
   if (y2Series.length > 0) {
     layout.yaxis2 = prepareYAxis(options.yAxis[1], options, y2Series);
     layout.yaxis2.overlaying = "y";

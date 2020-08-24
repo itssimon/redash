a3ae17531519f1bba17d4ea8b86cac6c812eb01a
Simon <simon@gurcke.de>
2020-08-24 18:09:03 +1000

    Fix tab size for Python

--- client/app/components/queries/QueryEditor/index.jsx
+++ client/app/components/queries/QueryEditor/index.jsx
@@ -141,7 +141,7 @@ const QueryEditor = React.forwardRef(function(
         editorProps={editorProps}
         width="100%"
         height="100%"
-        tabSize={2}
+        tabSize={syntax == "python" ? 4 : 2}
         setOptions={editorOptions}
         showPrintMargin={false}
         wrapEnabled={false}
